2031d97ea9e6af6fca102c8ea69a03b4dadccadf
cao truong <truongcmph15429@gmail.com>
2022-01-07 09:46:23 +0700

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double", { allowTemplateLiterals: true }],
+        indent: ["error", 4],
+    },
+};
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const show =[{
+    img:"https://picsum.photos/200/200",
+    name:"Vinh danh 295 sinh viên FPT POLYTECHNIC học kì spring 2018",
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo porro iusto, dolor magni libero unde.",
+},
+{img:"https://picsum.photos/200/200",
+    name:"Sinh viên khối ngành kinh tế học cách ''Chạm'' vào cảm xúc",
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo porro iusto, dolor magni libero unde.",
+},
+{
+    img:"https://picsum.photos/200/200",
+    name:"Sinh viên FPPLY trải nghiệm tức tế tại mường thanh LUXURY",
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo porro iusto, dolor magni libero unde.",
+}];
+
+export const menus=["Trang chủ","Tuyển sinh","Chương trình đào tạo","Góc sinh viên ","Tuyển dụng"];
+
